81a1f0c911ee4eab40ad4db907d19de35830a8ca
Deekline <46416799+Deekline@users.noreply.github.com>
2020-02-22 14:08:53 +0200

    update shop-data, add cartutil for card.reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
+import { useDispatch } from "react-redux";
+
+import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions";
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
-    </div>
-  </div>
-);
+const CollectionItem = ( props ) => {
+    const { name, price, imageUrl } = props.item;
+
+    const dispatch = useDispatch();
+    const addItemToCart = () => {
+        dispatch( addItem( props.item ) );
+    };
+
+    return (
+        <div className='collection-item'>
+            <div
+                className='image'
+                style={ {
+                    backgroundImage: `url(${ imageUrl })`
+                } }
+            />
+            <div className='collection-footer'>
+                <span className='name'>{ name }</span>
+                <span className='price'>{ price }</span>
+            </div>
+            <CustomButton onClick={ addItemToCart }>
+                Add to cart
+            </CustomButton>
+        </div>
+    );
+};
+
 
 export default CollectionItem;
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,8 +1,14 @@
 import { CartActionTypes } from "./cart.types";
-const { TOGGLE_CART_HIDDEN } = CartActionTypes;
+
+const { TOGGLE_CART_HIDDEN, ADD_ITEM } = CartActionTypes;
 
 
 export const toggleCartHidden = () => ({
     type: TOGGLE_CART_HIDDEN,
+});
+
+export const addItem = ( item ) => ({
+    type: ADD_ITEM,
+    payload: item
 });
 
