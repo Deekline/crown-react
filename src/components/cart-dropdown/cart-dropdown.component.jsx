b2b652d849af3f8c66d042c7a852ae266734067a
Deekline <46416799+Deekline@users.noreply.github.com>
2020-02-22 14:34:19 +0200

    update dropdown menu

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 
 import CustomButton from '../custom-button/custom-button.component';
+import { CartItem } from "../cart-item/cart-item.component";
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
-  <div className='cart-dropdown'>
-    <div className='cart-items' />
-    <CustomButton>GO TO CHECKOUT</CustomButton>
-  </div>
-);
+const CartDropdown = () => {
+    const items = useSelector( state => ({
+        cartItems: state.cart.cartItems
+    }) );
+    const { cartItems } = items;
 
+    return (
+        <div className='cart-dropdown'>
+            <div className='cart-items'>
+                { cartItems.map( item => <CartItem key={ item.id } item={ item }/> ) }
+            </div>
+            <CustomButton>GO TO CHECKOUT</CustomButton>
+        </div>
+    );
+};
 export default CartDropdown;
