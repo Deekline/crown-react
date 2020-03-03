90af290dfe30972fb138385598b0f270c6e67f55
Deekline <46416799+Deekline@users.noreply.github.com>
2020-03-03 22:55:01 +0200

    collections add  and data structure update

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../category/collections.component";
 
 
-const ShopPage = () => {
+const ShopPage = ( { match } ) => {
 
     return (
         <div className='shop-page'>
-            <CollectionsOverview/>
+            <Route exact path={ `${ match.path }` } component={ CollectionsOverview }/>
+            <Route path={ `${ match.path }/:categoryId` } component={ CollectionPage }/>
         </div>
     );
 };
-
 
 
 export default ShopPage;
